7ae263bee59f234174a0c38c2a7147ce388b65cd
Matthijs <matthijs@plamola.com>
2015-10-05 19:54:16 +0200

    Changed to EFF deeplink blog

--- src/test/scala/nl/dekkr/feedfrenzy/backend/ActionExecutorTest.scala
+++ src/test/scala/nl/dekkr/feedfrenzy/backend/ActionExecutorTest.scala
@@ -9,26 +9,25 @@ class ActionExecutorTest extends FlatSpecLike {
 
   val AE = new ActionExecutor()
 
-  val actionSplit = Action(order = 1, actionType = "split", inputVariable = None, outputVariable = Option("anchor"), template = Some("li.tour-of-scala > a"), replaceWith = None)
-  val actionSplitDA = Action(order = 1, actionType = "split", inputVariable = None, outputVariable = None, template = Some("div.deal-container >div.frame"), replaceWith = None)
+  val actionSplit = Action(order = 1, actionType = "split", inputVariable = None, outputVariable = Option("anchor"), template = Some("div.views-field-view-node > span.field-content > a"), replaceWith = None)
 
   val actionAttribute = Action(order = 2, actionType = "attribute", inputVariable = Option("anchor"), outputVariable = None, template = Some("href"), replaceWith = None)
 
 
   "ActionExecutor" should "succeed on a single split action" in {
-    val content = getFileAsString("doc.scala-lang.org.html")
+    val content = getFileAsString("eff-deeplinks-blog.html")
 
     val splitted = AE.start(content, List(actionSplit.copy(outputVariable = None)))
-    assert(splitted.length == 33)
+    assert(splitted.length == 8)
     splitted.foreach(a => assert(a.startsWith("<a")))
   }
 
  it should "succeed on a split / attribute sequence" in {
-    val content = getFileAsString("doc.scala-lang.org.html")
+    val content = getFileAsString("eff-deeplinks-blog.html")
 
     val attributed = AE.start(content, List(actionSplit, actionAttribute))
-    assert(attributed.length == 33)
-    attributed.foreach(a => assert(a.startsWith("http://")))
+    assert(attributed.length == 8)
+    attributed.foreach(a => assert(a.startsWith("https://")))
   }
 
   it should "return an empty list on a empty body" in {
@@ -38,14 +37,6 @@ class ActionExecutorTest extends FlatSpecLike {
     assert(attributed.isEmpty)
   }
 
-  it should "succeed on a split DA" in {
-    val content = getFileAsString("dagartikel.html")
-
-    val attributed = AE.start(content, List(actionSplitDA))
-    assert(attributed.length == 128)
-    attributed.foreach(println)
-    attributed.foreach(a => assert(a.startsWith("<div class=\"frame\">")))
-  }
 
 
   def getFileAsString(fileName: String): String = {
