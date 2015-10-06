ad7822ce60c3bb1f985870f0a2099bfebd87e2c4
Matthijs <matthijs@plamola.com>
2015-10-06 22:26:32 +0200

    Added fields for tags, createdDate & updatedDate
    implements dekkr/feedfrenzy-backend#2 partially

--- src/main/scala/nl/dekkr/feedfrenzy/backend/model/Article.scala
+++ src/main/scala/nl/dekkr/feedfrenzy/backend/model/Article.scala
@@ -1,6 +1,14 @@
 package nl.dekkr.feedfrenzy.backend.model
 
+import java.time.OffsetDateTime
 
-case class Article(uid: String, title: String, author: Option[String], content: String)
+
+case class Article(uid: String,
+                   title: String,
+                   author: Option[String] = None,
+                   content: String,
+                   createdDate: Option[OffsetDateTime] = None,
+                   updatedDate: Option[OffsetDateTime] = None,
+                   tags: List[String] = List.empty[String])
 
 
--- src/main/scala/nl/dekkr/feedfrenzy/backend/services/FrontendService.scala
+++ src/main/scala/nl/dekkr/feedfrenzy/backend/services/FrontendService.scala
@@ -16,7 +16,9 @@ import akka.stream.FlowMaterializer
 import akka.stream.scaladsl.{Flow, Sink, Source}
 import nl.dekkr.feedfrenzy.backend.model._
 import nl.dekkr.feedfrenzy.backend.util.{ArticleExtractor, ArticleLinksExtractor}
-import spray.json.DefaultJsonProtocol
+import spray.json._
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter._
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
 
@@ -25,10 +27,33 @@ trait Protocols extends DefaultJsonProtocol {
   implicit val articleLinksRequest = jsonFormat3(ArticleLinksRequest.apply)
   implicit val articleRequest = jsonFormat3(ArticleRequest.apply)
   implicit val articleLinks = jsonFormat1(ArticleLinks.apply)
-  implicit val article = jsonFormat4(Article.apply)
+  implicit val article = jsonFormat7(Article.apply)
   implicit val rawVariable = jsonFormat2(RawVariable.apply)
   implicit val rawVariables = jsonFormat2(RawVariables.apply)
   implicit val newContent = jsonFormat1(NewContent.apply)
+
+
+  implicit object OffsetDateTimeJsonFormat extends RootJsonFormat[OffsetDateTime] {
+    override def write(obj: OffsetDateTime) = JsString(obj.format(ISO_OFFSET_DATE_TIME))
+    override def read(json: JsValue): OffsetDateTime = json match {
+      case JsString(s) => OffsetDateTime.parse(s)
+      case _ => throw new DeserializationException("Could not parse date/time")
+    }
+  }
+
+  implicit object OptionOffsetDateTimeJsonFormat extends RootJsonFormat[Option[OffsetDateTime]] {
+    override def write(obj: Option[OffsetDateTime]) = obj match {
+      case None => JsNull
+      case Some(dt) =>   JsString(dt.format(ISO_OFFSET_DATE_TIME))
+    }
+    override def read(json: JsValue) : Option[OffsetDateTime] = json match {
+      case JsString(s) =>  Some(OffsetDateTime.parse(s))
+      case _ => throw new DeserializationException("Could not parse date/time")
+    }
+  }
+
+
+
 }
 
 trait FrontendService extends Protocols with Configuration {
