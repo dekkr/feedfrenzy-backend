b423211af9cc4bc7503b2ac906d85a29a1c41f47
Matthijs <matthijs@plamola.com>
2016-01-06 20:50:00 +0100

    Finished MockPageFetcher

--- src/test/scala/nl/dekkr/feedfrenzy/backend/test/MockPageFetcher.scala
+++ src/test/scala/nl/dekkr/feedfrenzy/backend/test/MockPageFetcher.scala
@@ -1,5 +1,7 @@
 package nl.dekkr.feedfrenzy.backend.test
 
+import java.net.UnknownHostException
+
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.StatusCodes._
@@ -12,7 +14,6 @@ import org.slf4j.LoggerFactory
 
 import scala.concurrent.Future
 import scala.language.postfixOps
-import scala.util.{Failure, Success, Try}
 import scalaj.http.HttpOptions
 
 
@@ -38,23 +39,29 @@ class MockPageFetcher {
     ))
 
   val requestHandler: akka.http.scaladsl.model.HttpRequest => akka.http.scaladsl.model.HttpResponse = {
-    case HttpRequest(HttpMethods.GET, uri, _, _, _) if uri.path.startsWith(Uri.Path("/v1/page")) => {
+    case HttpRequest(HttpMethods.GET, uri, _, _, _) if uri.path.startsWith(Uri.Path("/v1/page")) =>
       uri.query.get("url") match {
         case Some(url: String) =>
-          Try(collectPageContent(url, USER_AGENT)) match {
-            case Success(content) =>
-              akka.http.scaladsl.model.HttpResponse(entity = HttpEntity(MediaTypes.`text/html`, content.asString.body))
-            case Failure(e) => dummyResponse
+          try {
+            val content = collectPageContent(url, USER_AGENT)
+            akka.http.scaladsl.model.HttpResponse(entity = HttpEntity(MediaTypes.`text/html`, content.asString.body))
+          } catch {
+            case e: UnknownHostException =>
+              // Not the best way to present the error, but matches current implementation of pagefetcher
+              HttpResponse(InternalServerError, entity = s"${e.getMessage}")
+            case e: Exception =>
+              HttpResponse(BadRequest, entity = s"${e.getMessage}")
           }
         case None => dummyResponse
       }
-    }
     case _: akka.http.scaladsl.model.HttpRequest =>
       akka.http.scaladsl.model.HttpResponse(NotFound, entity = "Unknown resource")
   }
 
 
-  private def collectPageContent(uri: String, userAgent: String) = scalaj.http.Http(uri).option(HttpOptions.followRedirects(shouldFollow = true)).header("User-Agent", userAgent)
+  private def collectPageContent(uri: String, userAgent: String) =
+    scalaj.http.Http(uri).option(HttpOptions.followRedirects(shouldFollow = true)).header("User-Agent", userAgent)
+
 
   def startApi(): Unit = {
     logger.info(s"Enabling Mock pagefetcher...")
